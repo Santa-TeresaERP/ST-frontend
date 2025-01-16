198f974025b307df76d8e6c78ae5c112fd17237e
Mamani Ayala Oscar Diego <oscar.mamani@tecsup.edu.pe>
2025-01-16 08:28:51 -0500

    refactor/refactor remaked hooks of user

--- src/modules/user-creations/hook/useUsers.ts
+++ src/modules/user-creations/hook/useUsers.ts
@@ -1,41 +1,42 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { fetchUsers, createUser, updateUser, deleteUser } from '../action/user';
-import { UpdateUserPayload, CreateUserPayload, User } from '../types/user';
-import { useUserStore } from '@/core/store/auth';
+import { CreateUserPayload, UpdateUserPayload, User } from '@/modules/user-creations/types/user';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { deleteUser, getUser, updateUser, createUser } from '../action/user';
+import { fetchUsers } from '@/modules/user-creations/action/user';
 
-export const useUsers = () => {
-  const { setUsers } = useUserStore();
-  const queryClient = useQueryClient();
-
-  const { data, isLoading } = useQuery({
+export const useFetchUsers = () => {
+  return useQuery<User[], Error>({
     queryKey: ['users'],
     queryFn: fetchUsers,
-    select: (data) => {
-      setUsers(data);
-      return data;
-    },
   });
+};
 
-  const createUserMutation = useMutation<User, Error, CreateUserPayload>({
+export const useFetchUser = (id: string) => {
+  return useQuery<User, Error>({
+    queryKey: ['user', id],
+    queryFn: () => getUser(id),
+  });
+};
+
+export const useCreateUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation<User, Error, CreateUserPayload>({
     mutationFn: createUser,
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['users'] }),
   });
+};
 
-  const updateUserMutation = useMutation<User, Error, { id: number; payload: UpdateUserPayload }>({
+export const useUpdateUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation<User, Error, { id: string; payload: UpdateUserPayload }>({
     mutationFn: ({ id, payload }) => updateUser(id, payload),
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['users'] }),
   });
+};
 
-  const deleteUserMutation = useMutation<void, Error, number>({
+export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation<void, Error, string>({
     mutationFn: deleteUser,
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['users'] }),
   });
-
-  return {
-    users: data,
-    isLoading,
-    createUser: createUserMutation.mutateAsync,
-    updateUser: updateUserMutation.mutateAsync,
-    deleteUser: deleteUserMutation.mutateAsync,
-  };
 };
