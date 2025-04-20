612b3a33b4796b1ba8b95deae9fd109e4f03b332
ceczzx <geraldo123villanueva@gmail.com>
2025-04-19 22:12:21 -0500

    fix:changes for production

---
+++ src/app/pages/dashboard/produccion/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ProductionList from '../../../../modules/production/components/production-component-view';
+
+export default function ProductionListPage() {
+  return (
+    <div>
+      <ProductionList />
+    </div>
+  );
+}
