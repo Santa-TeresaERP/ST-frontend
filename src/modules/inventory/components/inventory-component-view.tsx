ef7d1e9e5d8fbf13e6493738ee64ca8240a25b3d
valeskaYcp <valeska.condori@tecsup.edu.pe>
2025-05-21 00:27:51 -0500

    Creacion de carpetas y conexion vista de inventario

---
+++ src/modules/inventory/components/inventory-component-view.tsx
@@ -0,0 +1,10 @@
+const InventoryComponentView = () => {
+    return (
+      <div>
+        <h1>Vista de Inventario</h1>
+      </div>
+    );
+  };
+  
+  export default InventoryComponentView;
+  
