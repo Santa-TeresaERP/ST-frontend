726f7cb85e78748f697a951b6c9ac8016df33c7c
CesarNunez1506 <cesar.nunez@tecsup.edu.pe>
2025-08-04 08:50:40 -0500

    Feat: Connection places

---
+++ src/modules/rentals/action/places.ts
@@ -0,0 +1,26 @@
+import api from '@/core/config/client';
+import { Place } from '../types/places';
+
+export const fetchPlaces = async (): Promise<Place[]> => {
+  const response = await api.get<Place[]>('/places');
+  return response.data;
+};
+
+export const fetchPlace = async (id: string): Promise<Place> => {
+  const response = await api.get<Place>(`/places/${id}`);
+  return response.data;
+};
+
+export const createPlace = async (payload: Omit<Place, '_id'>): Promise<Place> => {
+  const response = await api.post<Place>('/places', payload);
+  return response.data;
+};
+
+export const updatePlace = async (id: string, payload: Partial<Place>): Promise<Place> => {
+  const response = await api.patch<Place>(`/places/${id}`, payload);
+  return response.data;
+};
+
+export const deletePlace = async (id: string): Promise<void> => {
+  await api.delete(`/places/${id}`);
+};
